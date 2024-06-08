af292a394b0934172909fdb56bff1ab58bd6eca6
graceJoo <gracejoo26@gmail.com>
2024-06-08 11:39:24 +0900

    정규식 작성 방법-1

---
+++ main.js
@@ -0,0 +1,17 @@
+console.log(123);
+
+const str = `
+010-1234-5678
+thesecon@gmail.com
+https://www.omdbapi.com/?apikey=7035c60c&s=frozen
+The quick brown fox jumps over the lazy dog.
+aabbccdd
+`;
+
+//생성자
+const regexp = new RegExp("the", "gi");
+console.log(str.match(regexp));
+
+//리터럴
+const regexp2 = /the/gi;
+console.log(str.match(regexp2));
